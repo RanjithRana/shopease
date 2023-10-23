114f14d948853b97acc6f13b23f2e60de473b66b
Ranjith M <mranjith.cse@gmail.com>
2023-10-23 12:35:50 +0530

    updated resonsive styles

--- src/app/shared/moke-data/data.ts
+++ src/app/shared/moke-data/data.ts
@@ -28,7 +28,7 @@ export const products = [
     name: 'One Plus 8',
     category: 200,
     price: 1000,
-    description:'RAM 4GB | ROM 512GM | Screen Size 13.3',
+    description:'RAM 1GB | ROM 64GM | Screen Size 6.7',
     imgUrl:
       'https://images.pexels.com/photos/10343717/pexels-photo-10343717.jpeg',
   },
@@ -38,7 +38,7 @@ export const products = [
     name: 'Sony Dslr Camera SP1500',
     category: 300,
     price: 1000,
-    description:'RAM 4GB | ROM 512GM | Screen Size 13.3',
+    description:'RAM 1GB | ROM 12GM | Camera 12MP',
     imgUrl:
       'https://images.pexels.com/photos/45889/camera-photo-camera-sony-alpha-7-sony-45889.jpeg',
   },
@@ -48,7 +48,7 @@ export const products = [
     name: 'Apple MacBook pro',
     category: 100,
     price: 3500,
-    description:'RAM 4GB | ROM 512GM | Screen Size 13.3',
+    description:'RAM 2GB | ROM 256GM | Screen Size 15.6',
     imgUrl: 'https://images.pexels.com/photos/18105/pexels-photo.jpg',
   },
 
@@ -57,7 +57,7 @@ export const products = [
     name: 'Canon Dslr 1500D Camera',
     category: 300,
     price: 500,
-    description:'RAM 4GB | ROM 512GM | Screen Size 13.3',
+    description:'RAM 1GB | ROM 12GM | Camera 16MP',
     imgUrl:
       'https://images.pexels.com/photos/51383/photo-camera-subject-photographer-51383.jpeg',
   },
@@ -67,7 +67,7 @@ export const products = [
     name: 'Apple iPhone 15 pro max',
     category: 200,
     price: 999,
-    description:'RAM 4GB | ROM 512GM | Screen Size 13.3',
+    description:'RAM 2GB | ROM 64GM | Screen Size 6.7',
     imgUrl:
       'https://images.pexels.com/photos/16005007/pexels-photo-16005007/free-photo-of-apple-iphone-14-pro-max-mobile-phone.jpeg',
   },
@@ -77,7 +77,7 @@ export const products = [
     name: 'Canon Dslr 1500D Camera',
     category: 300,
     price: 899,
-    description:'RAM 4GB | ROM 512GM | Screen Size 13.3',
+    description:'RAM 1GB | ROM 12GM | Camera 12MP',
     imgUrl:
       'https://images.pexels.com/photos/51383/photo-camera-subject-photographer-51383.jpeg',
   },
@@ -86,7 +86,7 @@ export const products = [
     name: 'Dell Laptop Inspiron 15',
     category: 100,
     price: 599,
-    description:'RAM 4GB | ROM 512GM | Screen Size 13.3',
+    description:'RAM 2GB | ROM 512GM | Screen Size 15',
     imgUrl:
       'https://images.pexels.com/photos/1266982/pexels-photo-1266982.jpeg',
   },
@@ -96,7 +96,7 @@ export const products = [
     name: 'Panasonic Camera 15XX',
     category: 300,
     price: 399,
-    description:'RAM 4GB | ROM 512GM | Screen Size 13.3',
+    description:'RAM 512MB | ROM 8GM | Camera 8MP',
     imgUrl:
       'https://images.pexels.com/photos/1203803/pexels-photo-1203803.jpeg',
   },
--- src/app/views/base/header/header.component.ts
+++ src/app/views/base/header/header.component.ts
@@ -1,16 +1,25 @@
-import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, HostListener, QueryList, ViewChild, ViewChildren } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {
+export class HeaderComponent implements AfterViewInit{
   menuState: boolean = false;
   @ViewChild('hamburger')
   hamburger!: ElementRef;
+  @ViewChildren('menuLink') menuLinks!: QueryList<ElementRef>;
 
   constructor(private el: ElementRef) {}
+
+  ngAfterViewInit(): void {
+    this.menuLinks.forEach((item: ElementRef) => {
+      item.nativeElement.addEventListener('click', () => {
+        this.menuState = false;
+      });
+    });
+  }
 
   @HostListener('document:click', ['$event'])
   onClick(event: MouseEvent) {
